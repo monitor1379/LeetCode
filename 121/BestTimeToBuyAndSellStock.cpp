34abdb6996ced3949ea242b158dc2debc815751a
monitor1379 <yy4f5da2@hotmail.com>
2017-11-21 13:38:26 +0800

    <feat> add 121/BestTimeToBuyAndSellStock.

---
+++ 121/BestTimeToBuyAndSellStock.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <limits>
+
+using namespace std;
+
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minPrice = INT_MAX;
+        int profit = 0;
+        for(int i = 0; i < prices.size(); ++i)
+        {
+            if(prices[i] < minPrice)
+                minPrice = prices[i];
+            else if(prices[i] - minPrice > profit)
+                profit = prices[i] - minPrice;
+        }
+        return profit;
+
+    }
+};
+
+void display_vector(vector<int> v)
+{
+    for(size_t i = 0; i < v.size(); ++i)
+        cout << v[i] << ", ";
+    cout << endl;
+}
+
+int main()
+{
+    vector<int> nums = {7, 1, 5, 3, 6, 4};
+    display_vector(nums);
+    Solution s;
+    cout << s.maxProfit(nums) << endl;
+    return 0;
+}
