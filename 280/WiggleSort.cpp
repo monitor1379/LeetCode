1882780612314b5781af274c58621528a6f1cded
monitor1379 <yy4f5da2@hotmail.com>
2017-12-01 13:39:05 +0800

    <feature> add new approach to 280/WiggleSort

--- 280/WiggleSort.cpp
+++ 280/WiggleSort.cpp
@@ -7,6 +7,15 @@ using namespace std;
 // https://leetcode.com/problems/wiggle-sort/description/
 
 template<typename T>
+void display_2d_vector(vector<vector<T>> &v) {
+    for (vector<T> vi: v) {
+        for (T i: vi)
+            cout << i << ", ";
+        cout << endl;
+    }
+}
+
+template<typename T>
 void display_vector(vector<T> &v) {
     for (T i: v)
         cout << i << ", ";
@@ -26,24 +35,23 @@ public:
      * Time complexity:  O(nlogn)
      * Space complexity: O(1)
      */
-    void wiggleSort2(vector<int>& nums) {
-        if(nums.empty()) return ;
+    void wiggleSort2(vector<int> &nums) {
+        if (nums.empty()) return;
         sort(nums.begin(), nums.end());
-        for(int i = 1; i < nums.size() - 1; i += 2)
+        for (int i = 1; i < nums.size() - 1; i += 2)
             swap(nums[i], nums[i + 1]);
     }
 
     /*
      * Approach #2: One-pass swap.
      *
-     *
-     * TODO
-     * TODO
-     * TODO
-     * TODO
      */
-    void wiggleSort(vector<int>& nums) {
-        // TODO
+    void wiggleSort(vector<int> &nums) {
+        if (nums.size() <= 1) return;
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            if ((i % 2 == 0) == nums[i] > nums[i + 1])
+                swap(nums[i], nums[i + 1]);
+        }
     }
 
 };
