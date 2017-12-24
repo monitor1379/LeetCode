3cffafb18fd55cb370664b69b69c8bf8d2cb486d
monitor1379 <yy4f5da2@hotmail.com>
2017-12-24 22:32:08 +0800

    <feature> add 258/AddDigits

---
+++ 258/AddDigits.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// https://leetcode.com/problems/add-digits/description/
+
+class Solution {
+public:
+    int addDigits(int num) {
+        return (num != 0 && num % 9 == 0) ? 9 : num % 9;
+    }
+};
+
+int main() {
+    Solution s;
+    cout << s.addDigits(38) << endl;
+    cout << s.addDigits(27) << endl;
+    cout << s.addDigits(0) << endl;
+    return 0;
+}
