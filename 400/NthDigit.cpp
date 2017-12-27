7e23725f464730fcac957c7801b7b666ef9df857
monitor1379 <yy4f5da2@hotmail.com>
2017-12-27 16:29:40 +0800

    <feature> add 400/NthDigit

---
+++ 400/NthDigit.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+
+using namespace std;
+
+// https://leetcode.com/problems/nth-digit/description/
+
+template<typename T>
+string toString(T num) {
+    stringstream ss;
+    ss << num;
+    return ss.str();
+}
+
+class Solution {
+public:
+    int findNthDigit(int n) {
+        int t = 9;
+        long long len = 1;
+        int start = 1;
+
+        // 1. 找到nth digit所在范围，如1-9,10-99,100-999
+        while (n - t * len > 0) {
+            n -= t * len;
+            t *= 10;
+            len++;
+            start *= 10;
+        }
+
+        // 2. 找到nth digit所在的确切数值
+        int num = (n - 1) / len + start;
+
+        // 3. 找到nth digit在该数值中的位置
+        int k = (n - 1) % len;
+
+        return toString(num)[k] - '0';
+    }
+};
+
+int main() {
+
+    Solution s;
+    cout << s.findNthDigit(3) << endl;
+    cout << s.findNthDigit(2147483647) << endl;
+
+    return 0;
+}
